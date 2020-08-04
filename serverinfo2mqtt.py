987ffc0b4d608af0448a68c8deeea410bb03b01a
Troels Agergaard Jacobsen <troels@ajonline.dk>
2020-08-04 21:11:39 +0200

    added basic utilization

--- serverinfo2mqtt.py
+++ serverinfo2mqtt.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python
 
-import paho.mqtt.client as mqtt
 import configparser
+import paho.mqtt.client as mqtt
+import psutil
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    topic  = config['config']['topic']
+    disk  = config['config']['disk']
+    username  = config['mqtt-server']['username']
+    password  = config['mqtt-server']['password']
+    mqtt_host = config['mqtt-server']['host']
+    mqtt_port = int(config['mqtt-server']['port'])
+    client_id = config['mqtt-server']['client_id']
+    ca_certs  = config['mqtt-server']['ca_certs']
+
+    mqtt_client = mqtt.Client(client_id=client_id)
+    mqtt_client.username_pw_set(username, password=password)
+    mqtt_client.tls_set(ca_certs=ca_certs)
+    mqtt_client.connect(mqtt_host, mqtt_port, 60)
+    
+    attributes = "{"
+    attributes += "\"cpu-usage\": {},".format(psutil.cpu_percent())
+    attributes += "\"cpu-temp\": {},".format(max([x.current for x in psutil.sensors_temperatures()["k10temp"]]))
+    attributes += "\"mem-usage\": {},".format(psutil.virtual_memory().percent)
+    attributes += "\"swap-usage\": {},".format(psutil.swap_memory().percent)
+    attributes += "\"disk-usage\": {}".format(psutil.disk_usage(disk).percent)
+    attributes += "}"
+    
+    mqtt_client.publish(topic, payload=attributes)
+
+    mqtt_client.disconnect()
